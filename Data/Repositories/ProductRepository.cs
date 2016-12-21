dba0c9b9d0d9d5532b95e726dccb4bf816140fd0
DESKTOP-V19GB3J\Admin <lymaodt@gmail.com>
2016-12-21 22:19:32 +0700

    Initial database

---
+++ Data/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Data.Infrastructure;
+using Model.Models;
+
+namespace Data.Repositories
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+    }
+
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(DbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
