e030de229aa720c82dbcbfdf8435a1965832667f
DESKTOP-V19GB3J\Admin <lymaodt@gmail.com>
2017-01-03 14:47:43 +0700

    add PostService and PostCategoryService

---
+++ Data/Repositories/FeedbackRepository.cs
@@ -0,0 +1,16 @@
+﻿using Data.Infrastructure;
+using Model.Models;
+
+namespace Data.Repositories
+{
+    public interface IFeedbackRepository : IRepository<Feedback>
+    {
+    }
+
+    public class FeedbackRepository : RepositoryBase<Feedback>, IFeedbackRepository
+    {
+        public FeedbackRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
