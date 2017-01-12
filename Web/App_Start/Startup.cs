24e55e90e44c419ab3bc11c40aef0c802de92260
DESKTOP-V19GB3J\Admin <lymaodt@gmail.com>
2017-01-12 16:42:23 +0700

    webApi

---
+++ Web/App_Start/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Autofac;
+using Service;
+using System.Web.Http;
+using System.Web.Mvc;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Data.Infrastructure;
+using System.Reflection;
+using Data;
+using Data.Repositories;
+
+[assembly: OwinStartup(typeof(Web.App_Start.Startup))]
+
+namespace Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigAutofac(app);
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+
+        private void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly()); //Register WebApi Controllers
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<ShopDbContext>().AsSelf().InstancePerRequest();
+
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(PostCategoryRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Services
+            builder.RegisterAssemblyTypes(typeof(PostCategoryService).Assembly)
+               .Where(t => t.Name.EndsWith("Service"))
+               .AsImplementedInterfaces().InstancePerRequest();
+
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container); //Set the WebApi DependencyResolver
+
+        }
+    }
+}
---
+++ Web/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web.Models;
+
+namespace Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+
+            {
+
+                cfg.CreateMap<PostCategory, PostCategoryViewModel>();
+
+                cfg.CreateMap<Post, PostViewModel>();
+
+                cfg.CreateMap<PostTag, PostTagViewModel>();
+
+                cfg.CreateMap<Tag, TagViewModel>();
+
+            });
+            //Mapper.CreateMap<PostCategory, PostCategoryViewModel>();
+            //Mapper.CreateMap<PostCategory, PostCategoryViewModel>();
+            //Mapper.CreateMap<Tag, TagViewModel>();
+        }
+    }
+}
