2ce3dd31830e8a6e7744a59218b85df6a17fcd15
DESKTOP-V19GB3J\Admin <lymaodt@gmail.com>
2017-01-20 10:45:30 +0700

    edit productcategory

--- Web/Api/ProductCategoryController.cs
+++ Web/Api/ProductCategoryController.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Web.Infrastructure.Core;
+using Web.Infrastructure.Extensions;
 using Web.Models;
 
 namespace Web.Api
@@ -16,12 +17,41 @@ namespace Web.Api
     public class ProductCategoryController : ApiControllerBase
     {
         IProductCategoryService _productCategoryService;
-        public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService) :base(errorService)
+        public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService) : base(errorService)
         {
             this._productCategoryService = productCategoryService;
         }
+        [Route("getallparents")]
+        [HttpGet]
+        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productCategoryService.GetAll();
+
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                return response;
+            });
+        }
+
+        [Route("getbyid/{id:int}")]
+        [HttpGet]
+        public HttpResponseMessage GetById(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productCategoryService.GetById(id);
+                var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(model);
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                return response;
+            });
+        }
+
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request,string keyword, int page, int pageSize = 20)
+        [HttpGet]
+        public HttpResponseMessage GetAll(HttpRequestMessage request, string keyword, int page, int pageSize = 20)
         {
             return CreateHttpResponse(request, () =>
              {
@@ -41,5 +71,58 @@ namespace Web.Api
                  return response;
              });
         }
+        [Route("create")]
+        [HttpPost]
+        public HttpResponseMessage Create(HttpRequestMessage request, ProductCategoryViewModel productCategoryVM)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var newProductCategory = new ProductCategory();
+                    newProductCategory.UpdateProductCategory(productCategoryVM);
+                    _productCategoryService.Add(newProductCategory);
+                    _productCategoryService.Save();
+
+                    var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(newProductCategory);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+
+                return response;
+
+            });
+        }
+
+        [Route("update")]
+        [HttpPut]
+        public HttpResponseMessage Update(HttpRequestMessage request,ProductCategoryViewModel productCategoryVM)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    return request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var dbProductCategory = _productCategoryService.GetById(productCategoryVM.ID);
+                    dbProductCategory.UpdateProductCategory(productCategoryVM);
+                    dbProductCategory.UpdatedDate = DateTime.Now;
+                    _productCategoryService.Update(dbProductCategory);
+                    _productCategoryService.Save();
+
+                    var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(dbProductCategory);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+                return response;
+            });
+        }
+
     }
 }
--- Web/app/shared/services/apiService.js
+++ Web/app/shared/services/apiService.js
@@ -1,4 +1,4 @@
-﻿//<reference path="D:\CSharp\shop\Web\Assets/admin/libs/angular/angular.js" />
+﻿///<reference path="D:\CSharp\shop\Web\Assets/admin/libs/angular/angular.js" />
 (function (app) {
 
     app.factory('apiService', apiService);
@@ -7,8 +7,11 @@
 
     function apiService($http) {
         return {
-            get:get
+            get: get,
+            post: post,
+            put:put
         }
+
         function get(url, params, success, failure) {
             $http.get(url, params).then(function (result) {
                 success(result);
@@ -16,5 +19,37 @@
                 failure(error);
             });
         }
+
+        function post(url, data, success, failure) {
+            $http.post(url, data).then(function (result) {
+                success(result);
+            }, function (error) {
+                console.log(error.status)
+                if (error.status === 401) {
+                    notificationService.displayError('Authenticate is required.');
+                }
+                else if (failure != null) {
+                    failure(error);
+                }
+
+            });
+        }
+
+        function put(url, data, success, failure) {
+            $http.put(url, data).then(function (result) {
+                success(result);
+            }, function (error) {
+                console.log(error.status)
+                if (error.status === 401) {
+                    notificationService.displayError('Authenticate is required.');
+                }
+                else if (failure != null) {
+                    failure(error);
+                }
+
+            });
+        }
+
+
     }
 })(angular.module('shop.common'));
