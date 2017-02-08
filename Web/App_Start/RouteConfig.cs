9437e0268bf715e68b504e5201840d5e227e6828
DESKTOP-V19GB3J\Admin <lymaodt@gmail.com>
2017-02-08 16:34:48 +0700

    config route and add <module></module> element in Web.config file to run route

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Web
@@ -13,11 +9,38 @@ namespace Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //Các trang tĩnh trước
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                     name: "Login",
+                     url: "dang-nhap.html",
+                     defaults: new { controller = "Account", action = "Index", id = UrlParameter.Optional },
+                     namespaces: new string[] { "Web.Controllers" });
+
+            routes.MapRoute(
+                      name: "About",
+                      url: "gioi-thieu.html",
+                      defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional },
+                      namespaces: new string[] { "Web.Controllers" });
+
+            //Các trang có tham số truyền vào sau
+            routes.MapRoute(
+                        name: "Product",
+                        url: "{alias}.pc-{id}.html",
+                        defaults: new { controller = "Product", action = "Index", id = UrlParameter.Optional },
+                        namespaces: new string[] { "Web.Controllers" });
+
+            routes.MapRoute(
+                    name: "ProductDetail",
+                    url: "{alias}.p-{productId}.html",
+                    defaults: new { controller = "Product", action = "Detail", productId = UrlParameter.Optional },
+                    namespaces: new string[] { "Web.Controllers" });
+
+            //Trang mặc định sau cùng
+            routes.MapRoute(
+                 name: "Default",
+                 url: "{controller}/{action}/{id}",
+                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+             );
         }
     }
 }
