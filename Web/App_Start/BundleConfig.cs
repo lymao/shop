761ec356d54781b5bbbc97442d85f4ffed18ae4e
DESKTOP-V19GB3J\Admin <lymaodt@gmail.com>
2017-03-10 22:24:01 +0700

    edit bai37a do not use Bundle file

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -9,28 +9,6 @@ namespace Web
         public static void RegisterBundles(BundleCollection bundles)
         {
 
-
-            bundles.Add(new ScriptBundle("~/css/base").Include(
-                        "~/Assets/client/css/bootstrap.css",
-                        "~/Assets/admin/libs/jquery-ui/themes/black-tie/jquery-ui.min.css",
-                        "~/Assets/client/css/etalage.css",
-                        "~/Assets/client/css/custom.css",
-                        "~/Assets/client/css/style.css"
-                        ));
-
-
-            bundles.Add(new ScriptBundle("~/js/plugins").Include(
-                      "~/Assets/admin/libs/jquery-ui/jquery-ui.min.js",
-                      "~/Assets/client/js/jquery.etalage.min.js",
-                      "~/Assets/admin/libs/mustache/mustache.js",
-                      "~/Assets/admin/libs/numeral/numeral.js",
-                      "~/Assets/admin/libs/jquery-validation/dist/jquery.validate.js",
-                      "~/Assets/admin/libs/jquery-validation/dist/additional-methods.js",
-                      "~/Assets/client/js/controller/shoppingCart.js",
-                      "~/Assets/client/js/common.js",
-                      "~/Assets/client/js/jquery.flexisel.js"
-                      ));
-
             //// Use the development version of Modernizr to develop with and learn from. Then, when you're
             //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
